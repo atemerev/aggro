132fbceb4ea6aae67bdc5dbf934b7858f4d3fe23
Alexander Temerev <temerev@miriamlaurel.com>
2016-07-07 00:05:59 +0200

    Receiving TradeCaptureReport through FIX

--- src/main/scala/com/miriamlaurel/aggro/fix/FixHandler.scala
+++ src/main/scala/com/miriamlaurel/aggro/fix/FixHandler.scala
@@ -1,10 +1,17 @@
 package com.miriamlaurel.aggro.fix
 
+import java.time.Instant
+import java.util.UUID
+
+import com.miriamlaurel.aggro.model.Fill
+import com.miriamlaurel.fxcore.instrument.CurrencyPair
+import com.miriamlaurel.fxcore.portfolio.{Position, PositionSide}
 import quickfix.MessageCracker.Handler
-import quickfix.{Application, Message, SessionID}
+import quickfix.field.Side
+import quickfix.{Application, Message, SessionID, fix44}
 import quickfix.fix44.{MessageCracker, TradeCaptureReport}
 
-class FixHandler extends MessageCracker with Application {
+class FixHandler(reportListener: (Fill) => ()) extends MessageCracker with Application {
   override def fromAdmin(message: Message, sessionId: SessionID): Unit = ???
 
   override def onLogon(sessionId: SessionID): Unit = ???
@@ -20,5 +27,19 @@ class FixHandler extends MessageCracker with Application {
   override def toAdmin(message: Message, sessionId: SessionID): Unit = ???
 
   @Handler
-  def onExec(message: TradeCaptureReport, sid: SessionID): Unit = ???
+  def onExec(message: TradeCaptureReport, sid: SessionID): Unit = {
+    val symbol = message.getSymbol.getValue
+    val instrument = CurrencyPair(symbol)
+    val tradeQty = BigDecimal.valueOf(message.getLastQty.getValue)
+    val price = BigDecimal.valueOf(message.getLastPx.getValue)
+    val sideGrp = new fix44.TradeCaptureReport.NoSides
+    message.getGroup(1, sideGrp)
+    val side = if (sideGrp.getSide.getValue == Side.BUY) PositionSide.Long else PositionSide.Short
+    val orderId = sideGrp.getOrderID.getValue
+    val tradeId = message.getExecID.getValue
+    val position = Position(instrument, price, if (side == PositionSide.Long) tradeQty else -tradeQty, None, Instant.now(), UUID.randomUUID())
+    val fill = Fill(position, orderId, Some(tradeId), None) // todo populate inventory
+    reportListener.apply(fill)
+  }
+
 }
