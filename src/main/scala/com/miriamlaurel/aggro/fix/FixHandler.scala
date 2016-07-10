509d2b9a3d127f2ce83862e2aae421bb8e416b3d
Alexander Temerev <temerev@miriamlaurel.com>
2016-07-10 14:53:24 +0200

    TradeCaptureReport receiver

--- src/main/scala/com/miriamlaurel/aggro/fix/FixHandler.scala
+++ src/main/scala/com/miriamlaurel/aggro/fix/FixHandler.scala
@@ -3,15 +3,17 @@ package com.miriamlaurel.aggro.fix
 import java.time.Instant
 import java.util.UUID
 
+import com.miriamlaurel.aggro.Inventory
 import com.miriamlaurel.aggro.model.Fill
+import com.miriamlaurel.fxcore.asset.Currency
 import com.miriamlaurel.fxcore.instrument.CurrencyPair
 import com.miriamlaurel.fxcore.party.Party
 import com.miriamlaurel.fxcore.portfolio.{Position, PositionSide}
 import com.typesafe.scalalogging.LazyLogging
 import quickfix.MessageCracker.Handler
 import quickfix.field.Side
-import quickfix.{Application, Message, SessionID, fix44}
 import quickfix.fix44.{MessageCracker, TradeCaptureReport}
+import quickfix.{Application, Message, SessionID}
 
 class FixHandler(reportListener: Function[Fill, Unit]) extends MessageCracker with Application with LazyLogging {
   override def fromAdmin(message: Message, sessionId: SessionID): Unit = ()
@@ -38,14 +40,25 @@ class FixHandler(reportListener: Function[Fill, Unit]) extends MessageCracker wi
     val instrument = CurrencyPair(symbol)
     val tradeQty = BigDecimal.valueOf(message.getLastQty.getValue)
     val price = BigDecimal.valueOf(message.getLastPx.getValue)
-    val sideGrp = new fix44.TradeCaptureReport.NoSides
+    val sideGrp = new TradeCaptureReport.NoSides
     message.getGroup(1, sideGrp)
     val side = if (sideGrp.getSide.getValue == Side.BUY) PositionSide.Long else PositionSide.Short
     val orderId = sideGrp.getOrderID.getValue
+    val partyGrp = new TradeCaptureReport.NoSides.NoPartyIDs
+    sideGrp.getGroup(1, partyGrp)
+    val party = partyGrp.getPartyID.getValue
     val tradeId = message.getExecID.getValue
     val position = Position(instrument, price, if (side == PositionSide.Long) tradeQty else -tradeQty, None, Instant.now(), UUID.randomUUID())
-    val venue = Party("OKCN") // todo extract from repeating group
-    val fill = Fill(venue, position, orderId, Some(tradeId), None) // todo populate inventory
+    val venue = Party(party)
+    val legGrp = new TradeCaptureReport.NoLegs
+    val balance = for (i <- 1 to message.getNoLegs.getValue) yield {
+      message.getGroup(i, legGrp)
+      val ccy = Currency(legGrp.getLegCurrency.getValue)
+      val qty = BigDecimal(legGrp.getLegQty.getValue)
+      ccy -> qty
+    }
+    val inventory: Inventory = balance.toMap
+    val fill = Fill(venue, position, orderId, Some(tradeId), Some(inventory))
     reportListener.apply(fill)
   }
 
